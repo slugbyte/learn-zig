6e2160eeaa5eee4198ccbb8a5baccb0f57d55130
Duncan Marsh <slugbyte@slugbyte.com>
2024-03-09 14:35:45 +0800

    a stack that can auto free/destory remaing values on deinit

---
+++ src/ds_stack.zig
@@ -0,0 +1,155 @@
+const std = @import("std");
+const util = @import("./util.zig");
+
+const AutoDestroy = enum {
+    Disabled,
+    Free,
+    Destroy,
+};
+
+/// A linked list based queue that can auto destroy/free values stored on deinit
+/// if stack is not empty
+pub fn Stack(comptime T: type, comptime auto_destory: AutoDestroy) type {
+    return struct {
+        head: ?*Node = null,
+        len: usize = 0,
+        allocator: std.mem.Allocator,
+
+        const Self = @This();
+        const Node = struct {
+            value: T,
+            next: ?*Node,
+        };
+
+        pub fn init(allocator: std.mem.Allocator) Self {
+            return .{
+                .allocator = allocator,
+            };
+        }
+
+        pub fn deinit(self: *Self) void {
+            while (self.head) |head| {
+                switch (auto_destory) {
+                    .Disabled => {},
+                    .Free => self.allocator.free(head.value),
+                    .Destroy => self.allocator.destroy(head.value),
+                }
+
+                self.head = head.next;
+                self.allocator.destroy(head);
+            }
+        }
+
+        pub fn push(self: *Self, value: T) !void {
+            const node = try self.allocator.create(Node);
+            node.* = .{
+                .value = value,
+                .next = self.head,
+            };
+
+            self.head = node;
+            self.len += 1;
+        }
+
+        pub fn pop(self: *Self) ?T {
+            if (self.head) |head| {
+                const result = head.value;
+                self.head = head.next;
+                self.allocator.destroy(head);
+                self.len -= 1;
+                return result;
+            } else {
+                return null;
+            }
+        }
+
+        pub fn peek(self: *Self) ?T {
+            if (self.head) |head| {
+                return head.value;
+            } else {
+                return null;
+            }
+        }
+    };
+}
+
+test "Stack No AutoDestroy" {
+    util.setTestName("Stack No AutoDestroy");
+    var stack = Stack(u8, AutoDestroy.Disabled).init(std.testing.allocator);
+    defer stack.deinit();
+
+    try util.isOk("peek null when empty", stack.peek() == null);
+    try util.isOk("pop null when empty", stack.pop() == null);
+    try util.isOk("len 0", stack.len == 0);
+
+    try stack.push(2);
+    try util.isOk("len 1", stack.len == 1);
+    try stack.push(4);
+    try util.isOk("len 2", stack.len == 2);
+    try stack.push(6);
+    try util.isOk("len 3", stack.len == 3);
+
+    try util.isOk("pop 6", stack.pop().? == 6);
+    try util.isOk("len 2", stack.len == 2);
+    try util.isOk("pop 4", stack.pop().? == 4);
+    try util.isOk("len 1", stack.len == 1);
+    try util.isOk("pop 2", stack.pop().? == 2);
+    try util.isOk("len 0", stack.len == 0);
+}
+
+test "Stack Free" {
+    util.setTestName("Stack Free");
+    var stack = Stack([]u8, AutoDestroy.Free).init(std.testing.allocator);
+    defer stack.deinit();
+
+    try util.isOk("peek null when empty", stack.peek() == null);
+    try util.isOk("pop null when empty", stack.pop() == null);
+    try util.isOk("len 0", stack.len == 0);
+
+    const a = try std.testing.allocator.alloc(u8, 5);
+    const b = try std.testing.allocator.alloc(u8, 6);
+    const c = try std.testing.allocator.alloc(u8, 7);
+    for (c) |*item| {
+        item.* = 80;
+    }
+
+    try stack.push(a);
+    try stack.push(b);
+    try stack.push(c);
+
+    const first = stack.pop().?;
+    defer std.testing.allocator.free(first);
+    try util.isOk("first len 7", first.len == 7);
+    try util.isOk("first[0] is 80", first[0] == 80);
+}
+
+test "Stack Destory" {
+    util.setTestName("Stack Destory");
+    const Point = struct {
+        x: u8,
+        y: u8,
+    };
+
+    var stack = Stack(*Point, AutoDestroy.Destroy).init(std.testing.allocator);
+    defer stack.deinit();
+
+    const a = try std.testing.allocator.create(Point);
+    a.* = .{ .x = 0, .y = 0 };
+
+    // auto destroy
+    const b = try std.testing.allocator.create(Point);
+    b.* = .{ .x = 1, .y = 2 };
+
+    // auto_destroy
+    const c = try std.testing.allocator.create(Point);
+    c.* = .{ .x = 3, .y = 4 };
+
+    try stack.push(a);
+    try stack.push(b);
+    try stack.push(c);
+
+    const first = stack.pop().?;
+    defer std.testing.allocator.destroy(first);
+    try util.isOk("first.x: is 3", first.x == 3);
+    try util.isOk("first.y: is 4", first.y == 4);
+}
