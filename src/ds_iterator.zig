9407fdf69c1e766419424f745ef882bae14dacab
Duncan Marsh <slugbyte@slugbyte.com>
2024-03-09 12:10:30 +0800

    Random Iterator with errors

--- src/ds_iterator.zig
+++ src/ds_iterator.zig
@@ -1,8 +1,75 @@
 const std = @import("std");
-
 const util = @import("./util.zig");
 
+// This iterator returns an error and must be used with a while {} |thing| else |err| {}
+const RandomIterator = struct {
+    seed: u64,
+    prng: std.rand.DefaultPrng,
+    count: ?usize = null,
+
+    pub fn init() RandomIterator {
+        const time: u128 = @bitCast(std.time.nanoTimestamp());
+        const seed: u64 = @truncate(time);
+        return .{
+            .seed = seed,
+            .prng = std.rand.DefaultPrng.init(seed),
+        };
+    }
+
+    pub fn initWithCount(count: usize) RandomIterator {
+        var result = RandomIterator.init();
+        result.count = count;
+        return result;
+    }
+
+    pub fn next(self: *RandomIterator) !u8 {
+        if (self.count) |*count| {
+            if (count.* > 0) {
+                count.* -= 1;
+                return self.prng.random().int(u8);
+            } else {
+                return error.RandomIteratorEmpty;
+            }
+        } else {
+            return self.prng.random().int(u8);
+        }
+    }
+};
+
+test "RandomIterator" {
+    util.setTestName("RandomIterator");
+
+    var randIter = RandomIterator.initWithCount(128);
+    var count: usize = 0;
+    const target = 88;
+    var lastRand: u8 = undefined;
+    while (randIter.next()) |rand| {
+        count += 1;
+        lastRand = rand;
+        if (rand == target) {
+            break;
+        }
+    } else |err| {
+        try util.isOk("count is 128", count == 128);
+        try util.isOk("target not hit", lastRand != target);
+        try util.isOk("error.RandomIteratorEmpty", err == error.RandomIteratorEmpty);
+        return;
+    }
+    try util.isOk("not yet 128", count < 128);
+    try util.isOk("target hit", lastRand == target);
+}
+
+// This iterator returns an optional number and can be used with a while |thing| {}
 pub fn RangeIterator(comptime T: type) type {
+
+    std.builtin.Type
+    switch (@typeInfo(T)) {
+        .Int, .Float => {},
+        else => {
+            @compileError("RandomIterator init error T must be a int or float type.");
+        },
+    }
+
     return struct {
         const Self = @This();
         from: T,
